8099b6142c49dc2c31dc50fcd783811d10ceff04
Vinnys Lins <vinnysflins@gmail.com>
2019-12-14 19:00:11 -0300

    add functions to handle network issues

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { log } from './utils/promise-helpers.js'
+import { log, retry, timeoutPromise } from './utils/promise-helpers.js'
 import './utils/array-helpers.js'
 import { notasService as service } from './nota/service.js'
 import { debounceTime, partialize, pipe, takeUntil } from './utils/operators.js'
@@ -9,8 +9,7 @@ const operations = pipe(
 )
 
 const action = operations(() =>
-  service
-    .sumItems('2143')
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
     .then(console.log)
     .catch(console.error)
 )
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -6,3 +6,22 @@ export const log = param => {
   console.log(param)
   return param
 }
+
+export const timeoutPromise = (milliseconds, promise) => {
+  const timeout = new Promise((resolve, reject) =>
+    setTimeout(() => reject(`Limite de ${milliseconds}ms excedido.`), milliseconds))
+
+  return Promise.race([timeout, promise])
+}
+
+export const delay = milliseconds => data =>
+  new Promise(resolve =>
+    setTimeout(() => resolve(data), milliseconds))
+
+export const retry = (retries, milliseconds, fn) =>
+  fn().catch(error =>
+    delay(milliseconds)().then(() =>
+      retries > 1
+        ? retry(--retries, milliseconds, fn)
+        : Promise.reject(error))
+  )
